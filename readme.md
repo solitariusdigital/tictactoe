d9d9263dbc4c44f9c4a42a5e1072fe0078063205
Pouyan Madanipour <pouyan.mad@gmail.com>
2020-01-10 13:37:10 +1100

    submission

---
+++ readme.md
@@ -0,0 +1,33 @@
+# TIC TAC TOE
+Click here to play: https://poy4n.github.io/tictactoe/
+# Background
+Tic Tac Toe is considered one of the ancient games in human history. I myself have played it many times in different contexts and methods. This project provided such a great excitement and challenge to develop this game the way I may have wanted as a kid. 
+# How it works
+The game starts with P1 making the first move with X and P2 makes the next move with O. Players can keep playing as scores are recorded and shown for each round and set on the screen. Each set is the best of 5 rounds. When players want to leave the match they can enter their name with number of sets they have won in order to record the results.
+# Modes
+The board consits of six modes including the classic X and O. Players can switch between modes as they are compiting to complete each round and set. The set score do not reset unless the page is refreshed.
+
+1. Classic mode 
+2. Future mode
+3. Magic mode 
+4. Romantic mode 
+5. Season mode 
+6. Moon mode 
+# Development
+1. HTML
+2. CSS
+3. Javascript
+
+I started to sketch the layout on iPad with some color coding in order to design the board, score counters and buttons. Then I started to design the the board using HTML and CSS before starting to code the game algorithm in Javascript. This process gave me an opportunity to encounter problems as I was visually experiencing and examining the layout and fine tuning the pseudocode. The visual representation enhanced the ability to find problems and coding challenges as I was developing the algorithm in Javascript.
+# Challenges & Lessons
+To be honest all aspects of the process from inseption to development was a fun and an extreme challenge.
+Some to mention are as follow:
+
+1. Having an accurate pseudocode is extremely beneficial.
+2. Developer's tool is such a powerful tool to detect issues and investiagte solutions.
+3. Having organised codes and styling classes are major factors to speed up the design process.
+4. Resolve each problem at peace and step by step.
+5. Learnt so many new concepts, methods and fucnctions during this development.
+6. As game grew bigger, better coding solutions were needed which resulted in amending the rest of the code as new bugs surfaced. Such a stellar learning process as new problems are to be solved from a new perspective.
+
+### Hope you enjoy the game and please leave me your feedback
--- tictactoe.js
+++ tictactoe.js
@@ -3,18 +3,20 @@ let boxes = document.querySelectorAll('.square')
 let btnReset = document.querySelector('.reset')
 let xCount = document.querySelector('.score1') // X score count
 let oCount = document.querySelector('.score2') // O score count
+let xRound = document.querySelector('.round1')
+let oRound = document.querySelector('.round2')
 let message =  document.querySelector('.message')
 let colorTurn1 = document.querySelector('.p1')
 let colorTurn2 = document.querySelector('.p2')
-let mode = document.querySelector('.modes')
-let classic = document.querySelector('.classic')
-let future = document.querySelector('.future')
-let romantic = document.querySelector('.romantic')
-let player1 = 'X'
-let player2 = 'O'
+let mode = document.querySelector('.mode')
+let turn = 'X';
+let player1icon = 'icon1';
+let player2icon = 'icon2';
 let click = 0;
 let xScore = 0;
 let oScore = 0;
+let xR = 0;
+let oR = 0;
 
 // all boxes
 let box1 = document.querySelector('#a1')
@@ -30,20 +32,25 @@ let box9 = document.querySelector('#a9')
 // game click and turn
 
 boxes.forEach(function(box) {
+                colorTurn1.style.backgroundColor = 'black'
     box.addEventListener('click', function(event) {
-        if (event.target.textContent === '' && message.textContent === '') {
-            event.target.textContent = 'X'
-            if (player1 === 'X') {
-                event.target.textContent = 'O'
-                colorTurn2.style.backgroundColor = 'lightgreen'
+        if (event.target.classList.contains(player1icon) === false && 
+            event.target.classList.contains(player2icon) === false &&
+            message.textContent === '') {
+            if (turn === 'X') {
+                turn = 'O'
+                colorTurn2.style.backgroundColor = 'black'
                 colorTurn1.style.backgroundColor = ''
+                event.target.classList.add(player1icon)
             }   else {
-                event.target.textContent = 'X'
-                colorTurn1.style.backgroundColor = 'lightgreen'
+                turn = 'X'
+                colorTurn1.style.backgroundColor = 'black'
                 colorTurn2.style.backgroundColor = ''
+                event.target.classList.add(player2icon)
             }
             click++
             gamePlay();
+            round();
         }
     })
 });
@@ -52,215 +59,302 @@ boxes.forEach(function(box) {
 
 let gamePlay = function () {
     if 
-    (box1.textContent === 'X' &&
-    box2.textContent === 'X' &&
-    box3.textContent === 'X') { 
+    (box1.classList.contains(player1icon) &&
+    box2.classList.contains(player1icon) &&
+    box3.classList.contains(player1icon)) { 
         box1.style.backgroundColor = 'lightgreen'
         box2.style.backgroundColor = 'lightgreen'
         box3.style.backgroundColor = 'lightgreen'
         xCount.textContent = xScore + 1
         xScore++
-        message.textContent = 'X WINS 💯'
+        message.textContent = '❌ WINS'
     } else if 
-    (box4.textContent === 'X' &&
-    box5.textContent === 'X' &&
-    box6.textContent === 'X') {
+    (box4.classList.contains(player1icon) &&
+    box5.classList.contains(player1icon) &&
+    box6.classList.contains(player1icon)) {
         box4.style.backgroundColor = 'lightgreen'
         box5.style.backgroundColor = 'lightgreen'
         box6.style.backgroundColor = 'lightgreen'
         xCount.textContent = xScore + 1
         xScore++
-        message.textContent = 'X WINS 💯'
+        message.textContent = '❌ WINS'
     } else if 
-    (box7.textContent === 'X' &&
-    box8.textContent === 'X' &&
-    box9.textContent === 'X') {
+    (box7.classList.contains(player1icon) &&
+    box8.classList.contains(player1icon) &&
+    box9.classList.contains(player1icon)) {
         box7.style.backgroundColor = 'lightgreen'
         box8.style.backgroundColor = 'lightgreen'
         box9.style.backgroundColor = 'lightgreen'
         xCount.textContent = xScore + 1
         xScore++
-        message.textContent = 'X WINS 💯'
+        message.textContent = '❌ WINS'
     } else if 
-    (box1.textContent === 'X' &&
-    box4.textContent === 'X' &&
-    box7.textContent === 'X') {
+    (box1.classList.contains(player1icon) &&
+    box4.classList.contains(player1icon) &&
+    box7.classList.contains(player1icon)) {
         box1.style.backgroundColor = 'lightgreen'
         box4.style.backgroundColor = 'lightgreen'
         box7.style.backgroundColor = 'lightgreen'
         xCount.textContent = xScore + 1
         xScore++
-        message.textContent = 'X WINS 💯'
+        message.textContent = '❌ WINS'
     } else if 
-    (box2.textContent === 'X' &&
-    box5.textContent === 'X' &&
-    box8.textContent === 'X') {
+    (box2.classList.contains(player1icon) &&
+    box5.classList.contains(player1icon) &&
+    box8.classList.contains(player1icon)) {
         box2.style.backgroundColor = 'lightgreen'
         box5.style.backgroundColor = 'lightgreen'
         box8.style.backgroundColor = 'lightgreen'
         xCount.textContent = xScore + 1
         xScore++
-        message.textContent = 'X WINS 💯'
+        message.textContent = '❌ WINS'
     } else if 
-    (box3.textContent === 'X' &&
-    box6.textContent === 'X' &&
-    box9.textContent === 'X') {
+    (box3.classList.contains(player1icon) &&
+    box6.classList.contains(player1icon) &&
+    box9.classList.contains(player1icon)) {
         box3.style.backgroundColor = 'lightgreen'
         box6.style.backgroundColor = 'lightgreen'
         box9.style.backgroundColor = 'lightgreen'
         xCount.textContent = xScore + 1
         xScore++
-        message.textContent = 'X WINS 💯'
+        message.textContent = '❌ WINS'
     } else if 
-    (box1.textContent === 'X' &&
-    box5.textContent === 'X' &&
-    box9.textContent === 'X') {
+    (box1.classList.contains(player1icon) &&
+    box5.classList.contains(player1icon) &&
+    box9.classList.contains(player1icon)) {
         box1.style.backgroundColor = 'lightgreen'
         box5.style.backgroundColor = 'lightgreen'
         box9.style.backgroundColor = 'lightgreen'
         xCount.textContent = xScore + 1
         xScore++
-        message.textContent = 'X WINS 💯'
+        message.textContent = '❌ WINS'
     } else if 
-    (box3.textContent === 'X' &&
-    box5.textContent === 'X' &&
-    box7.textContent === 'X') {
+    (box3.classList.contains(player1icon) &&
+    box5.classList.contains(player1icon) &&
+    box7.classList.contains(player1icon)) {
         box3.style.backgroundColor = 'lightgreen'
         box5.style.backgroundColor = 'lightgreen'
         box7.style.backgroundColor = 'lightgreen'
         xCount.textContent = xScore + 1
         xScore++
-        message.textContent = 'X WINS 💯'
+        message.textContent = '❌ WINS'
     } else if 
-    (box1.textContent === 'O' &&
-    box2.textContent === 'O' &&
-    box3.textContent === 'O') {
+    (box1.classList.contains(player2icon) &&
+    box2.classList.contains(player2icon) &&
+    box3.classList.contains(player2icon)) {
         box1.style.backgroundColor = 'lightgreen'
         box2.style.backgroundColor = 'lightgreen'
         box3.style.backgroundColor = 'lightgreen'
         oCount.textContent = oScore + 1
         oScore++
-        message.textContent = 'O WINS 💯'
+        message.textContent = '⭕️ WINS'
     } else if 
-    (box4.textContent === 'O' &&
-    box5.textContent === 'O' &&
-    box6.textContent === 'O') {
+    (box4.classList.contains(player2icon) &&
+    box5.classList.contains(player2icon) &&
+    box6.classList.contains(player2icon)) {
         box4.style.backgroundColor = 'lightgreen'
         box5.style.backgroundColor = 'lightgreen'
         box6.style.backgroundColor = 'lightgreen'
         oCount.textContent = oScore + 1
         oScore++
-        message.textContent = 'O WINS 💯'
+        message.textContent = '⭕️ WINS'
     } else if 
-    (box7.textContent === 'O' &&
-    box8.textContent === 'O' &&
-    box9.textContent === 'O') {
+    (box7.classList.contains(player2icon) &&
+    box8.classList.contains(player2icon) &&
+    box9.classList.contains(player2icon)) {
         box7.style.backgroundColor = 'lightgreen'
         box8.style.backgroundColor = 'lightgreen'
         box9.style.backgroundColor = 'lightgreen'
         oCount.textContent = oScore + 1
         oScore++
-        message.textContent = 'O WINS 💯'
+        message.textContent = '⭕️ WINS'
     } else if 
-    (box1.textContent === 'O' &&
-    box4.textContent === 'O' &&
-    box7.textContent === 'O') {
+    (box1.classList.contains(player2icon) &&
+    box4.classList.contains(player2icon) &&
+    box7.classList.contains(player2icon)) {
         box1.style.backgroundColor = 'lightgreen'
         box4.style.backgroundColor = 'lightgreen'
         box7.style.backgroundColor = 'lightgreen'
         oCount.textContent = oScore + 1
         oScore++
-        message.textContent = 'O WINS 💯'
+        message.textContent = '⭕️ WINS'
     } else if 
-    (box2.textContent === 'O' &&
-    box5.textContent === 'O' &&
-    box8.textContent === 'O') {
+    (box2.classList.contains(player2icon) &&
+    box5.classList.contains(player2icon) &&
+    box8.classList.contains(player2icon)) {
         box2.style.backgroundColor = 'lightgreen'
         box5.style.backgroundColor = 'lightgreen'
         box8.style.backgroundColor = 'lightgreen'
         oCount.textContent = oScore + 1
         oScore++
-        message.textContent = 'O WINS 💯'
+        message.textContent = '⭕️ WINS'
     } else if 
-    (box3.textContent === 'O' &&
-    box6.textContent === 'O' &&
-    box9.textContent === 'O') {
+    (box3.classList.contains(player2icon) &&
+    box6.classList.contains(player2icon) &&
+    box9.classList.contains(player2icon)) {
         box3.style.backgroundColor = 'lightgreen'
         box6.style.backgroundColor = 'lightgreen'
         box9.style.backgroundColor = 'lightgreen'
         oCount.textContent = oScore + 1
         oScore++
-        message.textContent = 'O WINS 💯'
+        message.textContent = '⭕️ WINS'
     } else if 
-    (box1.textContent === 'O' &&
-    box5.textContent === 'O' &&
-    box9.textContent === 'O') {
+    (box1.classList.contains(player2icon) &&
+    box5.classList.contains(player2icon) &&
+    box9.classList.contains(player2icon)) {
         box1.style.backgroundColor = 'lightgreen'
         box5.style.backgroundColor = 'lightgreen'
         box9.style.backgroundColor = 'lightgreen'
         oCount.textContent = oScore + 1
         oScore++
-        message.textContent = 'O WINS 💯'
+        message.textContent = '⭕️ WINS'
     } else if 
-    (box3.textContent === 'O' &&
-    box5.textContent === 'O' &&
-    box7.textContent === 'O') {
+    (box3.classList.contains(player2icon) &&
+    box5.classList.contains(player2icon) &&
+    box7.classList.contains(player2icon)) {
         box3.style.backgroundColor = 'lightgreen'
         box5.style.backgroundColor = 'lightgreen'
         box7.style.backgroundColor = 'lightgreen'
         oCount.textContent = oScore + 1
         oScore++
-        message.textContent = 'O WINS 💯'
+        message.textContent = '⭕️ WINS'
     } else if (click === 9) {
-        message.textContent = '🤡 DRAW!!! PLAY SMARTER 🤡'
+        message.textContent = '❌ D R A W ⭕️'
     }
 };
 
-// reset
+// round count
 
-let resetGame = function() {
-    boxes.forEach(function(text) {
-        text.textContent = '';
-    });
-    boxes.forEach(function(back) {
-        back.style.backgroundColor = '';
-    });
-    click = 0;
-    message.textContent = '';
+var round = function() {
+    if (xScore === 3) {
+        xCount.textContent = 0;
+        xRound.textContent = xR + 1;
+        xScore = 0;
+        xR++
+    }   
+    else if (oScore === 3) {
+        oCount.textContent = 0;
+        oRound.textContent = oR + 1;
+        oScore = 0;
+        oR++
+    }
 };
-
-btnReset.addEventListener('click', resetGame);
 
 // mode
 
 var changeMode = function() {
-    if (classic.textContent === 'classic') {
-        board.classList.toggle('boxmode')
-        document.body.classList.toggle('boxmode')
-    
-        playTurn1.classList.toggle('topmode')
-        playTurn2.classList.toggle('topmode')
-        xCount.classList.toggle('topmode')
-        oCount.classList.toggle('topmode')
-    }
-    else if (future.textContent === 'Mode 1') {
-        board.classList.toggle('boxmode')
-        document.body.classList.toggle('boxmode')
-        
-        playTurn1.classList.toggle('topmode')
-        playTurn2.classList.toggle('topmode')
-        xCount.classList.toggle('topmode')
-        oCount.classList.toggle('topmode')
-    } 
-    else if (romantic.textContent === 'Mode 2') {
-        board.classList.toggle('boxmode')
-        document.body.classList.toggle('boxmode')
+    if (mode.value === 'Classic') {
+        resetGame();
+        document.body.style.backgroundColor = ''
+        board.classList.remove('future')
+        board.classList.remove('season')
+        board.classList.remove('romantic')
+        board.classList.remove('moon')
+        board.classList.remove('magic')
 
-        playTurn1.classList.toggle('topmode')
-        playTurn2.classList.toggle('topmode')
-        xCount.classList.toggle('topmode')
-        oCount.classList.toggle('topmode')
+        player1icon = 'icon1' // apply class to player
+        player2icon = 'icon2'
+    } 
+    else if (mode.value === "Future") {
+        resetGame();
+        document.body.style.backgroundColor = 'black'
+        board.classList.add('future')
+        board.classList.remove('season')
+        board.classList.remove('romantic')
+        board.classList.remove('moon')
+        board.classList.remove('magic')
+
+        player1icon = 'icon3'
+        player2icon = 'icon4'
     }
-}
+    else if (mode.value === 'Season') {
+        resetGame();
+        document.body.style.backgroundColor = 'rgb(192, 241, 159)'
+        board.classList.add('season')
+        board.classList.remove('future')
+        board.classList.remove('romantic')
+        board.classList.remove('moon')
+        board.classList.remove('magic')
+
+        player1icon = 'icon5' 
+        player2icon = 'icon6' 
+    }
+    else if (mode.value === 'Romantic') {
+        resetGame();
+        document.body.style.backgroundColor = 'rgb(236, 172, 168)'
+        board.classList.add('romantic')
+        board.classList.remove('future')
+        board.classList.remove('season')
+        board.classList.remove('moon')
+        board.classList.remove('magic')
+
+        player1icon = 'icon7'
+        player2icon = 'icon8' 
+    }
+    else if (mode.value === 'Moon') {
+        resetGame();
+        document.body.style.backgroundColor = 'white'
+        board.classList.add('moon')
+        board.classList.remove('romantic')
+        board.classList.remove('future')
+        board.classList.remove('season')
+        board.classList.remove('magic')
+
+        player1icon = 'icon9'
+        player2icon = 'icon10' 
+    }
+    else if (mode.value === 'Magic') {
+        resetGame();
+        document.body.style.backgroundColor = 'rgb(189, 168, 206)'
+        board.classList.add('magic')
+        board.classList.remove('romantic')
+        board.classList.remove('future')
+        board.classList.remove('season')
+        board.classList.remove('moon')
+
+        player1icon = 'icon11'
+        player2icon = 'icon12' 
+    }
+};
 
 mode.addEventListener('change', changeMode);
+
+// local storage
+
+let inpKey = document.querySelector('#inpKey')
+let inpValue = document.querySelector('#inpValue')
+let btnInsert = document.querySelector('#btnInsert')
+let outPut = document.querySelector('#outPut')
+
+let collectData = function() {
+    let key = inpKey.value;
+    let value = inpValue.value;
+
+    if (key !== '' && value !== '') {
+        localStorage.setItem(key, value);
+        location.reload();
+    }
+};
+
+for (let i =0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    let value = localStorage.getItem(key);
+
+    outPut.innerHTML += `🏆 ${key} ${value}<br>`
+}
+
+btnInsert.addEventListener('click', collectData);
+
+// reset
+
+let resetGame = function() {
+    boxes.forEach(function(box) {
+        box.classList.remove(player1icon);
+        box.classList.remove(player2icon);
+        box.style.backgroundColor = '';
+    });
+    message.textContent = '';
+    click = 0;
+};
+
+btnReset.addEventListener('click', resetGame);
